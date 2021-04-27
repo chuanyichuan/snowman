067dd03bf44630dd3d15c8d1ce90c4f2e1929a9e
chuanchuan <lucunyu21@gmail.com>
2021-04-27 17:02:40 +0800

    sql script

--- script/DDL.sql
+++ script/DDL.sql
@@ -1,7 +1,20 @@
-drop table if exists sm_service_instance;
-create table sm_service_instance
+create table snowman.sm_group
 (
-    id          bigint auto_increment primary key,
+    id           bigint auto_increment
+        primary key,
+    name         varchar(200) not null comment '服务名称',
+    group_code   varchar(200) not null comment '服务组编号',
+    chunk        int          not null comment '服务组每次获取ID数量',
+    mode         int       default 1 null comment '1:数字;2:雪花算法;3:UUID',
+    `last_value` bigint       not null comment '服务组最近一次获取的ID最大值',
+    gmt_created  timestamp null,
+    gmt_updated  timestamp default CURRENT_TIMESTAMP null
+) comment '客户端服务组';
+
+create table snowman.sm_service_instance
+(
+    id          bigint auto_increment
+        primary key,
     group_id    bigint              not null comment '服务组ID',
     server_code varchar(200)        not null comment '服务编号',
     snow_times  int                 not null comment '获取ID次数',
@@ -10,21 +23,10 @@ create table sm_service_instance
     gmt_updated timestamp default CURRENT_TIMESTAMP null
 ) comment '客户端服务实例';
 
-drop table if exists sm_group;
-create table sm_group
+create table snowman.sm_snowflake
 (
-    id          bigint auto_increment primary key,
-    group_code  varchar(200) not null comment '服务组编号',
-    chunk       int          not null comment '服务组每次获取ID数量',
-    last_value  bigint       not null comment '服务组最近一次获取的ID最大值',
-    gmt_created timestamp null,
-    gmt_updated timestamp default CURRENT_TIMESTAMP null
-) comment '客户端服务组';
-
-drop table if exists sm_snowflake;
-create table sm_snowflake
-(
-    id                  bigint auto_increment primary key,
+    id                  bigint auto_increment
+        primary key,
     service_instance_id bigint not null comment '服务实例ID',
     chunk               int    not null comment '服务实例本次获取ID的数量',
     from_value          bigint not null comment 'ID起始值(含括)',
