a2d689e88a5917dd71cae3298b50318e636bc20a
chuanchuan <lucunyu21@gmail.com>
2021-05-13 17:13:29 +0800

    1. ThreadPoolHelper 2. ThreadPoolTaskExecutor

--- snowman-server/src/main/java/cc/kevinlu/snow/server/SnowmanApplication.java
+++ snowman-server/src/main/java/cc/kevinlu/snow/server/SnowmanApplication.java
@@ -2,8 +2,14 @@ package cc.kevinlu.snow.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import cc.kevinlu.snow.server.utils.helper.ThreadPoolHelper;
 
 @SpringBootApplication(scanBasePackages = { "cc.kevinlu.snow.server" })
 @EnableDiscoveryClient
@@ -12,6 +18,12 @@ public class SnowmanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SnowmanApplication.class, args);
+    }
+
+    @Bean(name = "taskExecutor")
+    @Primary
+    public ThreadPoolTaskExecutor taskExecutor(TaskExecutionProperties properties) {
+        return ThreadPoolHelper.newTaskThreadPool(properties);
     }
 
 }
---
+++ snowman-server/src/main/java/cc/kevinlu/snow/server/processor/task/CheckChunkProcessor.java
@@ -0,0 +1,16 @@
+package cc.kevinlu.snow.server.processor.task;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 检查剩余预生成数据是否充裕
+ * 
+ * @author chuan
+ */
+@Slf4j
+@Component
+public class CheckChunkProcessor {
+
+}
